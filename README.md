81b5000ab826ad9a6c2afae9c55231586e89d21d
eric soucy <eric.soucy@gmail.com>
2022-03-19 10:08:20 -0400

    cmd line runner

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This is using Java 17 and maven 3.8.4, the code is  [ceilingFan](./src/main/java
 tests are provided in [CeilingFanUnitTests.java](./src/test/java/org/example/CeilingFanUnitTests.java)
 
 ```bash
-// to run tests
+// to run tests and build package
 mvn verify
+
+// to run the application
+java -cp target\ceiling-fan-1.0-SNAPSHOT.jar org.example.CeilingFan
 ```
--- src/main/java/org/example/CeilingFan.java
+++ src/main/java/org/example/CeilingFan.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class CeilingFan {
   Direction direction;
   int speedSetting;
@@ -7,6 +11,45 @@ public class CeilingFan {
   CeilingFan() {
     this.speedSetting = 0;
     this.direction = Direction.NORMAL;
+  }
+
+  public static void printMenu(String[] options) {
+    for (String option : options) {
+      System.out.println(option);
+    }
+    System.out.print("Choose your option : ");
+  }
+
+  public static void main(String[] args) {
+    //
+    CeilingFan fan = new CeilingFan();
+    String[] options = {"D- Pull Direction Cord", "S- Pull Speed Cord", "Q- Quit Program"};
+    Scanner scanner = new Scanner(System.in);
+    String option = "";
+    while (!option.equalsIgnoreCase("q")) {
+      printMenu(options);
+      option = scanner.nextLine();
+      switch (option.toLowerCase()) {
+        case "d" -> {
+          fan.pullDirectionCord();
+          System.out.println(
+                  "Fan Direction: " + fan.getDirection() + " Fan Speed: " + fan.getSpeedSetting());
+          System.out.println();
+        }
+        case "s" -> {
+          fan.pullSpeedCord();
+          System.out.println(
+                  "Fan Direction: " + fan.getDirection() + " Fan Speed: " + fan.getSpeedSetting());
+          System.out.println();
+        }
+        case "q" -> exit(0);
+        default -> {
+          System.out.println("Unrecognized option");
+          System.out.println();
+
+        }
+      }
+    }
   }
 
   public void pullSpeedCord() {
